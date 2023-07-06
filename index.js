f01a769cf3e9f88e1e75ed59d868fd58433d7358
mutembeic <135004069+mutembeic@users.noreply.github.com>
2023-07-06 13:44:19 +0300

    done with styling the homepage

--- index.js
+++ index.js
@@ -1,63 +1,63 @@
-  // JavaScript
-   // Declare the shoesData variable outside the fetch request
-let shoesData;
-
-// Get the 'Men' link element
-const menLink = document.getElementById('menLink');
-
-// Add click event listener to 'Men' link
-menLink.addEventListener('click', displayMenShoes);
-
-// Function to display men's shoes
-function displayMenShoes() {
-  // Get the container element to display the shoes
-  const shoesContainer = document.getElementById('shoesContainer');
-
-  // Clear any existing content in the container
-  shoesContainer.innerHTML = '';
-
-  // Loop through the men's shoes data and create HTML elements
-  shoesData.forEach(shoe => {
-    // Create the shoe card element
-    const shoeCard = document.createElement('div');
-    shoeCard.classList.add('shoe-card');
-
-    // Create the shoe image element
-    const shoeImage = document.createElement('img');
-    shoeImage.src = shoe.image;
-    shoeImage.alt = shoe.name;
-
-    // Create the shoe name element
-    const shoeName = document.createElement('h3');
-    shoeName.textContent = shoe.name;
-
-    // Create the shoe price element
-    const shoePrice = document.createElement('p');
-    shoePrice.textContent = `Price: ${shoe.price}`;
-
-    // Append the elements to the shoe card
-    shoeCard.appendChild(shoeImage);
-    shoeCard.appendChild(shoeName);
-    shoeCard.appendChild(shoePrice);
-
-    // Append the shoe card to the container
-    shoesContainer.appendChild(shoeCard);
-  });
-
-  // Show the shoes container after rendering the shoes
-  shoesContainer.style.display = 'block';
-}
+let shoesData; // Declare the shoesData variable outside the fetch block
 
 fetch('http://localhost:3000/mens')
   .then(response => response.json())
   .then(data => {
-    // Store the fetched data in the shoesData variable
-    shoesData = data;
+    shoesData = data; // Assign the fetched data to the shoesData variable
+
+    // Get the 'Men' link element
+    const menLink = document.getElementById('menLink');
+
+    // Add click event listener to 'Men' link
+    menLink.addEventListener('click', displayMenShoes);
+
+    // Function to display men's shoes
+    function displayMenShoes() {
+      // Get the container element to display the shoes
+      const shoesContainer = document.getElementById('shoesContainer');
+
+      // Clear any existing content in the container
+      shoesContainer.innerHTML = '';
+
+      // Loop through the men's shoes data and create HTML elements
+      shoesData.forEach(shoe => {
+        // Create the shoe card element
+        const shoeCard = document.createElement('div');
+        shoeCard.classList.add('shoe-card');
+
+        // Create the shoe image element
+        const shoeImage = document.createElement('img');
+        shoeImage.src = shoe.image;
+        shoeImage.alt = shoe.name;
+
+        // Create the shoe name element
+        const shoeName = document.createElement('h3');
+        shoeName.textContent = shoe.name;
+
+        // Create the shoe price element
+        const shoePrice = document.createElement('p');
+        shoePrice.textContent = `Price: ${shoe.price}`;
+
+        // Append the elements to the shoe card
+        shoeCard.appendChild(shoeImage);
+        shoeCard.appendChild(shoeName);
+        shoeCard.appendChild(shoePrice);
+
+        // Append the shoe card to the container
+        shoesContainer.appendChild(shoeCard);
+      });
+
+      // Show the shoes container after rendering the shoes
+      shoesContainer.style.display = 'block';
+    }
+
+    // Rest of your code here...
   })
   .catch(error => {
     console.log('An error occurred while fetching data:', error);
   });
 
+// Rest of your code...
 const addToCartButtons = document.getElementsByClassName('btn');
 const popupMessage = document.getElementById('popup-message');
 
@@ -94,4 +94,35 @@ menuIcon.addEventListener('click', toggleMenuItems);
 
 
 
- 
+
+function displayMenShoes() {
+  const shoesContainer = document.getElementById('shoesContainer');
+  shoesContainer.innerHTML = '';
+
+  if (Array.isArray(shoesData)) {
+    shoesData.forEach(shoe => {
+      const shoeCard = document.createElement('div');
+      shoeCard.classList.add('shoe-card');
+
+      const shoeImage = document.createElement('img');
+      shoeImage.src = shoe.image;
+      shoeImage.alt = shoe.name;
+
+      const shoeName = document.createElement('h3');
+      shoeName.textContent = shoe.name;
+
+      const shoePrice = document.createElement('p');
+      shoePrice.textContent = `Price: ${shoe.price}`;
+
+      shoeCard.appendChild(shoeImage);
+      shoeCard.appendChild(shoeName);
+      shoeCard.appendChild(shoePrice);
+
+      shoesContainer.appendChild(shoeCard);
+    });
+  } else {
+    console.log('Error: Invalid shoes data');
+  }
+
+  shoesContainer.style.display = 'block';
+}
